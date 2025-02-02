35d1420ce4a830fea7f18327dfed7058ff1f03d4
Jason Jee <js768355@dal.ca>
2025-02-02 01:48:04 -0400

    Change button name

--- src/MealBuilder.js
+++ src/MealBuilder.js
@@ -336,7 +336,7 @@ const saveMeal = async () => {
               onChange={(e) => setPortions(e.target.value)}
             />
             <button className="divide-button" onClick={divideByPortions}>
-              Divide
+              Portion
             </button>
           </div>
         )}
